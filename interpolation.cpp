39515b49cbbe7372eddaef6dbaa351bdd3c27166
victor-rios03 <victor_rios03@protonmail.com>
2022-11-30 22:17:23 -0700

    Interpolacion de lagrange y lineal

--- interpolation.cpp
+++ interpolation.cpp
@@ -3,3 +3,40 @@
 //
 
 #include "interpolation.h"
+
+using namespace std;
+
+void linear_interpolation(const vector<Points>& points, double x) {
+    double y;
+    double m;
+
+    for (int i=0; i<points.size()-1; i++) {
+        if (x > points[i].x && x < points[i+1].x ){
+            cout << "El punto esta entre, (";
+            cout << points[i].x << ", " << points[i+1].x << ')' << endl;
+            m = (points[i+1].y - points[i].y) /  (points[i+1].x - points[i].x);
+            y = points[i].y + (x - points[i].x) * m;
+            cout << "Usando interpolacion lineal el punto: " << x << ", esta en :"<< y << endl;
+            return;
+        }
+    }
+
+    cout << "No se puede usar interpolacion lineal con los datos dados" << endl;
+}
+
+void lagrange_interpolation(const vector<Points>& points, double x) {
+    double L=0;
+    double L_temp;
+
+    for (int i=0; i<points.size(); i++) {
+        L_temp = points[i].y;
+        for (int j=0; j<points.size(); j++) {
+            if (j != i) {
+                L_temp *= (x - points[j].x) / (points[i].x - points[j].x);
+            }
+        }
+        cout << "L_(" << i << ") = " << L_temp << endl;
+        L += L_temp;
+    }
+    cout << "Usando interpolacion de lagrange el punto: " << x << ", esta en :"<< L << endl;
+}
--- interpolation.h
+++ interpolation.h
@@ -6,3 +6,17 @@
 #define INTERPOLATION_AND_REGRESSION_INTERPOLATION_H
 
 #endif //INTERPOLATION_AND_REGRESSION_INTERPOLATION_H
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+struct Points {    // points coordinates given by points.dat
+    double x;
+    double y;
+};
+
+void linear_interpolation(const vector<Points>&, double);
+
+void lagrange_interpolation(const vector<Points>&, double);
